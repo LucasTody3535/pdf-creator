4090c576fa23f6d1a5930f33847012922ff1e368
Toodie <lucas.vtodt@gmail.com>
2023-02-20 18:04:18 -0300

    add basic layout for app and some flex globals

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,24 @@
   <h1>Works</h1>
 </template>
 
-<style scoped></style>
+<style>
+#app {
+  display: grid;
+  height: 100vh;
+  width: 100vw;
+  grid-template-columns: 60% 40%;
+  grid-template-rows: 100%;
+}
+
+.flex-centered {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.flex-center-left {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+}
+</style>
