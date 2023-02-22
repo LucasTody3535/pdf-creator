23b4c312429425df5bd387c1e7ddb7597a75c0d7
Toodie <lucas.vtodt@gmail.com>
2023-02-21 21:46:33 -0300

    add util for conversion from pixel to pt

--- src/utils/conversion/conversion_utils.ts
+++ src/utils/conversion/conversion_utils.ts
@@ -1,3 +1,7 @@
 export function fromPixelToMM(value: number) {
     return Number((value * 0.2645833333));
 }
+
+export function fromPixelToPT(value: number) {
+    return Number((value * 0.75));
+}
